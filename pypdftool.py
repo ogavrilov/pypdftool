b0535bf8f99b2a993966d228a98f1f883c52d28f
Олег Гаврилов <o.gavrilov@ant-konsalt.ru>
2019-09-03 11:03:53 +0300

    add processing %newpdf% in input field as create new pdf with data from options

--- pypdftool.py
+++ pypdftool.py
@@ -11,65 +11,40 @@ from reportlab.pdfgen import canvas
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
 
-def prepareTextMod(optionsData):
-	# get input pdf
-	input_pdf = PdfFileReader(open(optionsData.get('inputFile'), "rb"))
-	# prepare new pdf
-	outputFile = optionsData.get('outputFile')
-	if outputFile is None:
-		outputFile = optionsData.get('inputFile')
-	output = PdfFileWriter()
-	# for each page
-	pageInd = 0
-	mod = optionsData.get('mod')
-	pageNumber = mod.get('pageNumber', -1)
-	for pageInd in range(input_pdf.getNumPages()):
-		curPage = input_pdf.getPage(pageInd)
-		if pageNumber == 0 or pageNumber == pageInd + 1:
-			pageHeight = curPage.mediaBox.getHeight()
-			pageWidth = curPage.mediaBox.getWidth()
-			# draw
-			packet = io.BytesIO()
-			can = canvas.Canvas(packet)
-			d = Drawing(pageHeight, pageWidth)
-			lab = Label()
-			lab.boxAnchor = 'ne'
-			lab.angle = mod.get('angle', 0)
-			lab.x = int(pageWidth * mod.get('left', 10) / 100)
-			lab.y = int(pageHeight - pageHeight * mod.get('top', 10) / 100)
-			lab.boxStrokeColor = colors.HexColor(mod.get('borderColor', '#000000'))
-			lab.boxStrokeWidth = mod.get('borderWidth', 0)
-			fontName = mod.get('fontName', 'Courier')
-			if mod.get('Bold', True):
-				fontName = fontName + '-Bold'
-			if fontName not in pdfmetrics.getRegisteredFontNames():
-				try:
-					pdfmetrics.registerFont(TTFont(fontName, fontName + '.ttf'))
-				except:
-					print('Warning: can not register font "' + fontName + '", use standart "Courier", check text encoding!')
-					fontName = pdfmetrics.getRegisteredFontNames()[0]
-			lab.fontName = fontName
-			lab.fontSize = mod.get('fontSize', 20)
-			lab.fillColor = colors.HexColor(mod.get('textColor', '#000000'))
-			textS = ''
-			for curLine in mod.get('text', []):
-				if len(textS) > 0:
-					textS += '\n'
-				textS += curLine
-			lab.setText(textS)
-			d.add(lab)
-			renderPDF.draw(d, can, 0, 0)
-			can.save()
-			packet.seek(0)
-			new_pdf = PdfFileReader(packet)
-			curPage.mergePage(new_pdf.getPage(0))
-		output.addPage(curPage)
-	outputStream = open(outputFile, "wb")
-	output.write(outputStream)
-	outputStream.close()
-
-def prepareImgMod(curMod):
-	a = 1
+def get_pdf_packet(mod, pageHeight = 400, pageWidth = 400):
+	packet = io.BytesIO()
+	can = canvas.Canvas(packet)
+	d = Drawing(pageHeight, pageWidth)
+	lab = Label()
+	lab.boxAnchor = 'ne'
+	lab.angle = mod.get('angle', 0)
+	lab.x = int(pageWidth * mod.get('left', 10) / 100)
+	lab.y = int(pageHeight - pageHeight * mod.get('top', 10) / 100)
+	lab.boxStrokeColor = colors.HexColor(mod.get('borderColor', '#000000'))
+	lab.boxStrokeWidth = mod.get('borderWidth', 0)
+	fontName = mod.get('fontName', 'Courier')
+	if mod.get('Bold', True):
+		fontName = fontName + '-Bold'
+	if fontName not in pdfmetrics.getRegisteredFontNames():
+		try:
+			pdfmetrics.registerFont(TTFont(fontName, fontName + '.ttf'))
+		except:
+			print('Warning: can not register font "' + fontName + '", use standart "Courier", check text encoding!')
+			fontName = pdfmetrics.getRegisteredFontNames()[0]
+	lab.fontName = fontName
+	lab.fontSize = mod.get('fontSize', 20)
+	lab.fillColor = colors.HexColor(mod.get('textColor', '#000000'))
+	textS = ''
+	for curLine in mod.get('text', []):
+		if len(textS) > 0:
+			textS += '\n'
+		textS += curLine
+	lab.setText(textS)
+	d.add(lab)
+	renderPDF.draw(d, can, 0, 0)
+	can.save()
+	packet.seek(0)
+	return packet
 	
 if __name__ == '__main__':
 	# check arg
@@ -83,7 +58,7 @@ if __name__ == '__main__':
 			optionsData = json.load(fileH)
 		# check input file
 		inputFile = optionsData.get('inputFile')
-		if inputFile is None or not os.path.exists(inputFile):
+		if inputFile is None or (inputFile != '%newpdf%' and not os.path.exists(inputFile)):
 			print('Can not find input type')
 		else:
 			# check mod
@@ -93,7 +68,42 @@ if __name__ == '__main__':
 			else:
 				# mod to input pdf
 				ModType = mod.get('type')
-				if not ModType is None and ModType == "addText":
-					prepareTextMod(optionsData)
-				elif not ModType is None and ModType == "addImg":
-					prepareImgMod(optionsData)
+				input_pdf_name = optionsData.get('inputFile')
+				if input_pdf_name == '%newpdf%':
+					outputFile = optionsData.get('outputFile')
+					mod = optionsData.get('mod')
+					# draw
+					if ModType == 'addText':
+						packet = get_pdf_packet(mod)
+					new_pdf = PdfFileReader(packet)
+					output = PdfFileWriter()
+					output.addPage(new_pdf.getPage(0))
+					outputStream = open(outputFile, "wb")
+					output.write(outputStream)
+					outputStream.close()
+				else:
+					# get input pdf
+					input_pdf = PdfFileReader(open(input_pdf_name, "rb"))
+					# prepare new pdf
+					outputFile = optionsData.get('outputFile')
+					if outputFile is None:
+						outputFile = input_pdf_name
+					output = PdfFileWriter()
+					# for each page
+					pageInd = 0
+					mod = optionsData.get('mod')
+					pageNumber = mod.get('pageNumber', -1)
+					for pageInd in range(input_pdf.getNumPages()):
+						curPage = input_pdf.getPage(pageInd)
+						if pageNumber == 0 or pageNumber == pageInd + 1:
+							pageHeight = curPage.mediaBox.getHeight()
+							pageWidth = curPage.mediaBox.getWidth()
+							# draw
+							if ModType == 'addText':
+								packet = get_pdf_packet(mod, pageHeight, pageWidth)
+								new_pdf = PdfFileReader(packet)
+								curPage.mergePage(new_pdf.getPage(0))
+						output.addPage(curPage)
+					outputStream = open(outputFile, "wb")
+					output.write(outputStream)
+					outputStream.close()
